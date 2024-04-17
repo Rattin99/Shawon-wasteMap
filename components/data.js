1d12aefce81f2f703b418a11b89835d56fc7a0ea
Rattin Sadman <rattin99m18@gmail.com>
2024-04-17 14:37:34 +0600

    first commit'

---
+++ components/data.js
@@ -0,0 +1,516 @@
+
+
+export const data = [
+    {
+        id: 1,
+        name: "Jubayer Ahmed",
+        latitude: 24.908705,
+        longitude: 91.835567,
+        last_cleaning_date: "No",
+        next_cleaning_date: "2/11/2035"
+      },
+      {
+        id: 2,
+        name: "Chowdhury Sadman",
+        latitude: 24.908567,
+        longitude: 91.835692,
+        last_cleaning_date: "No",
+        next_cleaning_date: "2/24/2019"
+      },
+      {
+        id: 3,
+        name: "Prof. Dr. Md. Shah Alam",
+        latitude: 24.907298,
+        longitude: 91.836508,
+        last_cleaning_date: "No",
+        next_cleaning_date: "5/24/2107"
+      },
+      {
+        id: 4,
+        name: "Shamsul Haque",
+        latitude: 24.907065,
+        longitude: 91.835456,
+        last_cleaning_date: "No",
+        next_cleaning_date: "6/20/2026"
+      },
+      {
+        id: 5,
+        name: "Md Helal Ahmed",
+        latitude: 24.907311,
+        longitude: 91.835629,
+        last_cleaning_date: "18-Jul",
+        next_cleaning_date: "11/2/2023"
+      },
+      {
+        id: 6,
+        name: "Mohammad Abdul Malik",
+        latitude: 24.90854,
+        longitude: 91.83556,
+        last_cleaning_date: "No",
+        next_cleaning_date: "11/16/2072"
+      },
+      {
+        id: 7,
+        name: "Anowar Khan",
+        latitude: 24.907934,
+        longitude: 91.836568,
+        last_cleaning_date: "No",
+        next_cleaning_date: "5/2/2055"
+      },
+      {
+        id: 8,
+        name: "Nipa Akhter",
+        latitude: 24.908098,
+        longitude: 91.835178,
+        last_cleaning_date: "No",
+        next_cleaning_date: "9/22/2145"
+      },
+      {
+        id: 9,
+        name: "Md kamal Uddin",
+        latitude: 24.908105,
+        longitude: 91.835317,
+        last_cleaning_date: "No",
+        next_cleaning_date: "1/14/2031"
+      },
+      {
+        id: 10,
+        name: "Mujammel Chowdhury",
+        latitude: 24.907466,
+        longitude: 91.835743,
+        last_cleaning_date: "21-Jan",
+        next_cleaning_date: "2/19/2032"
+      },
+      {
+        id: 11,
+        name: "Hazi Rafique Miah",
+        latitude: 24.907483,
+        longitude: 91.835603,
+        last_cleaning_date: "No",
+        next_cleaning_date: "12/4/2046"
+      },
+      {
+        id: 12,
+        name: "Rezia Begum",
+        latitude: 24.90775,
+        longitude: 91.83635,
+        last_cleaning_date: "No",
+        next_cleaning_date: "5/25/2032"
+      },
+      {
+        id: 13,
+        name: "Saju miah",
+        latitude: 24.907787,
+        longitude: 91.836207,
+        last_cleaning_date: "No",
+        next_cleaning_date: "10/30/2033"
+      },
+      {
+        id: 14,
+        name: "SM Shahjahan",
+        latitude: 24.907593,
+        longitude: 91.836547,
+        last_cleaning_date: "No",
+        next_cleaning_date: "3/13/2028"
+      },
+      {
+        id: 15,
+        name: "Aminur Rahman",
+        latitude: 24.907522,
+        longitude: 91.835335,
+        last_cleaning_date: "No",
+        next_cleaning_date: "9/11/2044"
+      },
+      {
+        id: 16,
+        name: "Dr. Yunus",
+        latitude: 24.907702,
+        longitude: 91.835214,
+        last_cleaning_date: "No",
+        next_cleaning_date: "10/20/2055"
+      },
+      {
+        id: 17,
+        name: "Amirun Nesa",
+        latitude: 24.907678,
+        longitude: 91.835526,
+        last_cleaning_date: "22-Jan",
+        next_cleaning_date: "2/20/2024"
+      },
+      {
+        id: 18,
+        name: "Dr. SSIH Jalali",
+        latitude: 24.906924,
+        longitude: 91.836274,
+        last_cleaning_date: "No",
+        next_cleaning_date: "4/22/2150"
+      },
+      {
+        id: 19,
+        name: "XX",
+        latitude: 24.908335,
+        longitude: 91.83607,
+        last_cleaning_date: "No",
+        next_cleaning_date: "10/8/2035"
+      },
+      {
+        id: 20,
+        name: "YY",
+        latitude: 24.90747,
+        longitude: 91.83656,
+        last_cleaning_date: "No",
+        next_cleaning_date: "8/20/2044"
+      },
+      {
+        id: 21,
+        name: "Moffazzol Khan",
+        latitude: 24.90734,
+        longitude: 91.83674,
+        last_cleaning_date: "15-Oct",
+        next_cleaning_date: "10/7/2071"
+      },
+      {
+        id: 22,
+        name: "Nayma Shirin Alam",
+        latitude: 24.907048,
+        longitude: 91.836134,
+        last_cleaning_date: "No",
+        next_cleaning_date: "11/1/2037"
+      },
+      {
+        id: 23,
+        name: "ZZ",
+        latitude: 24.90706,
+        longitude: 91.83531,
+        last_cleaning_date: "No",
+        next_cleaning_date: "2/20/2033"
+      },
+      {
+        id: 24,
+        name: "Abdul Malek",
+        latitude: 24.907289,
+        longitude: 91.835367,
+        last_cleaning_date: "No",
+        next_cleaning_date: "1/10/2030"
+      },
+      {
+        id: 25,
+        name: "Dr Mohammad Razaul Karim",
+        latitude: 24.907521,
+        longitude: 91.838976,
+        last_cleaning_date: "No",
+        next_cleaning_date: "3/5/2017"
+      },
+      {
+        id: 25,
+        name: "Dr Mohammad Razaul Karim",
+        latitude: 24.907521,
+        longitude: 91.838976,
+        last_cleaning_date: "No",
+        next_cleaning_date: "3/5/2017"
+      },
+      {
+        id: 26,
+        name: "Shamim Ahmed",
+        latitude: 24.90764,
+        longitude: 91.839057,
+        last_cleaning_date: "15-Oct",
+        next_cleaning_date: "5/22/2073"
+      },
+      {
+        id: 28,
+        name: "Adv. Md Azim Uddin",
+        latitude: 24.907578,
+        longitude: 91.838278,
+        last_cleaning_date: "18-Jun",
+        next_cleaning_date: "3/15/2037"
+      },
+      {
+        id: 29,
+        name: "Abdur Razzak",
+        latitude: 24.907545,
+        longitude: 91.837874,
+        last_cleaning_date: "20-Jun",
+        next_cleaning_date: "7/7/2032"
+      },
+      {
+        id: 30,
+        name: "Abulles",
+        latitude: 24.907607,
+        longitude: 91.838098,
+        last_cleaning_date: "No",
+        next_cleaning_date: "8/27/2141"
+      },
+      {
+        id: 31,
+        name: "Muhammad Shamsul Arefin",
+        latitude: 24.907385,
+        longitude: 91.837986,
+        last_cleaning_date: "No",
+        next_cleaning_date: "1/12/2068"
+      },
+      {
+        id: 32,
+        name: "Abdul Kayum",
+        latitude: 24.90722,
+        longitude: 91.83781,
+        last_cleaning_date: "20-Jan",
+        next_cleaning_date: "4/22/2027"
+      },
+      {
+        id: 33,
+        name: "Abdul Khalik",
+        latitude: 24.907133,
+        longitude: 91.838675,
+        last_cleaning_date: "23-Jan",
+        next_cleaning_date: "11/4/2032"
+      },
+      {
+        id: 34,
+        name: "Mirza Shapan",
+        latitude: 24.907266,
+        longitude: 91.839593,
+        last_cleaning_date: "No",
+        next_cleaning_date: "11/28/2031"
+      },
+      {
+        id: 35,
+        name: "Raja Mia",
+        latitude: 24.907162,
+        longitude: 91.837683,
+        last_cleaning_date: "No",
+        next_cleaning_date: "7/22/2009"
+      },
+      {
+        id: 37,
+        name: "Rajia Khanum",
+        latitude: 24.906957,
+        longitude: 91.838702,
+        last_cleaning_date: "20-Jun",
+        next_cleaning_date: "10/22/2025"
+      },
+      {
+        id: 38,
+        name: "Dr MS Rahman Shamim",
+        latitude: 24.906911,
+        longitude: 91.839005,
+        last_cleaning_date: "No",
+        next_cleaning_date: "4/19/2067"
+      },
+      {
+        id: 39,
+        name: "Rustom Ali",
+        latitude: 24.90654,
+        longitude: 91.838843,
+        last_cleaning_date: "No",
+        next_cleaning_date: "9/20/2054"
+      },
+      {
+        id: 40,
+        name: "Samsul Islam",
+        latitude: 24.906477,
+        longitude: 91.839506,
+        last_cleaning_date: "No",
+        next_cleaning_date: "10/20/2025"
+      },
+      {
+        id: 40,
+        name: "Samsul Islam",
+        latitude: 24.906477,
+        longitude: 91.839506,
+        last_cleaning_date: "No",
+        next_cleaning_date: "10/20/2025"
+      },
+      {
+        id: 41,
+        name: "Motiur Rahman",
+        latitude: 24.906448,
+        longitude: 91.839631,
+        last_cleaning_date: "No",
+        next_cleaning_date: "2/14/2019"
+      },
+      {
+        id: 42,
+        name: "Tamim Ur Rashid",
+        latitude: 24.9066,
+        longitude: 91.83813,
+        last_cleaning_date: "14-May",
+        next_cleaning_date: "8/20/2137"
+      },
+      {
+        id: 43,
+        name: "Sabur Ullah",
+        latitude: 24.906414,
+        longitude: 91.837732,
+        last_cleaning_date: "16-Jan",
+        next_cleaning_date: "7/16/2044"
+      },
+      {
+        id: 44,
+        name: "Rabia Begum",
+        latitude: 24.90636,
+        longitude: 91.837378,
+        last_cleaning_date: "20-Jul",
+        next_cleaning_date: "7/11/2036"
+      },
+      {
+        id: 45,
+        name: "Prof Dr M. A. Hasnat",
+        latitude: 24.906358,
+        longitude: 91.837252,
+        last_cleaning_date: "No",
+        next_cleaning_date: "12/22/2055"
+      },
+      {
+        id: 46,
+        name: "Mahmud and others",
+        latitude: 24.906591,
+        longitude: 91.837243,
+        last_cleaning_date: "20-Oct",
+        next_cleaning_date: "12/12/2048"
+      },
+      {
+        id: 47,
+        name: "Ful Mia",
+        latitude: 24.90573,
+        longitude: 91.837106,
+        last_cleaning_date: "15-Jan",
+        next_cleaning_date: "2/17/2252"
+      },
+      {
+        id: 48,
+        name: "Razia Begum",
+        latitude: 24.90629,
+        longitude: 91.83799,
+        last_cleaning_date: "No",
+        next_cleaning_date: "11/18/2016"
+      },
+      {
+        id: 49,
+        name: "Md liakot Mia",
+        latitude: 24.905992,
+        longitude: 91.837641,
+        last_cleaning_date: "No",
+        next_cleaning_date: "3/29/2037"
+      },
+      {
+        id: 50,
+        name: "Sushanto Singho",
+        latitude: 24.905998,
+        longitude: 91.837515,
+        last_cleaning_date: "No",
+        next_cleaning_date: "2/21/2047"
+      },
+      {
+        id: 51,
+        name: "Dr. Begum Lutfur Nahar Chinu",
+        latitude: 24.905998,
+        longitude: 91.837068,
+        last_cleaning_date: "No",
+        next_cleaning_date: "1/29/2042"
+      },
+      {
+        id: 52,
+        name: "Majid",
+        latitude: 24.966208,
+        longitude: 91.838543,
+        last_cleaning_date: "15-May",
+        next_cleaning_date: "3/8/2019"
+      },
+      {
+        id: 53,
+        name: "Yusuf mia",
+        latitude: 24.905916,
+        longitude: 91.838848,
+        last_cleaning_date: "23-Jan",
+        next_cleaning_date: "11/14/2030"
+      },
+      {
+        id: 54,
+        name: "Anowar ali",
+        latitude: 24.905875,
+        longitude: 91.839238,
+        last_cleaning_date: "20-Jan",
+        next_cleaning_date: "12/8/2036"
+      },
+      {
+        id: 55,
+        name: "Juhurul Islam Chowdhury",
+        latitude: 24.906031,
+        longitude: 91.839646,
+        last_cleaning_date: "No",
+        next_cleaning_date: "7/12/2020"
+      },
+      {
+        id: 56,
+        name: "Akhtaruzzaman",
+        latitude: 24.906024,
+        longitude: 91.839752,
+        last_cleaning_date: "No",
+        next_cleaning_date: "4/23/2042"
+      },
+      {
+        id: 57,
+        name: "Muhammad Khairul",
+        latitude: 24.905985,
+        longitude: 91.839971,
+        last_cleaning_date: "No",
+        next_cleaning_date: "9/6/2125"
+      },
+      {
+        id: 58,
+        name: "Abul Khayer Muhammad",
+        latitude: 24.905955,
+        longitude: 91.840077,
+        last_cleaning_date: "21-Dec",
+        next_cleaning_date: "8/26/2377"
+      },
+      {
+        id: 59,
+        name: "Kanak prova sarkar",
+        latitude: 24.905707,
+        longitude: 91.840194,
+        last_cleaning_date: "No",
+        next_cleaning_date: "2/21/2045"
+      },
+      {
+        id: 60,
+        name: "Abdul Akhter",
+        latitude: 34.905613,
+        longitude: 91.837634,
+        last_cleaning_date: "14-Feb",
+        next_cleaning_date: "11/22/2035"
+      },
+      {
+        id: 61,
+        name: "Sujat Ahmed",
+        latitude: 24.905808,
+        longitude: 91.837783,
+        last_cleaning_date: "18-Sep",
+        next_cleaning_date: "5/30/2032"
+      },
+      {
+        id: 62,
+        name: "Jitu Miah",
+        latitude: 24.905834,
+        longitude: 91.837334,
+        last_cleaning_date: "No",
+        next_cleaning_date: "1/12/2023"
+      },
+      {
+        id: 63,
+        name: "Dr. Afaz Uddin",
+        latitude: 24.90581,
+        longitude: 91.837046,
+        last_cleaning_date: "No",
+        next_cleaning_date: "1/14/2038"
+      },
+      {
+        id: 64,
+        name: "Kumru Miah",
+        latitude: 24.905681,
+        longitude: 91.839554,
+        last_cleaning_date: "No",
+        next_cleaning_date: "3/25/2024"
+      }
+]
