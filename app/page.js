297f3cd7d6faa0261d4ca5f72ae6a21ed3914058
Rattin99 <rattin67@student.sust.edu>
2024-05-12 03:55:01 +0600

    change

--- app/page.js
+++ app/page.js
@@ -13,23 +13,34 @@ import {
 import { data } from "@/components/data";
 import { useState } from "react";
 
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 export default function Home() {
 
+  const notify = () => toast("House id does not exist in database.\nplease enter a number between 1 and 62");
   const [markers,setMarkers] = useState(data);
   const [open,setOpen] = useState(false);
   const [selected, setSelected] = useState(data[0]);
-  const [search,setSearh] = useState(null);
 
   const position = {lat:markers[0].latitude, lng: markers[0].longitude};
 
-  const handleSearch = () => {
+  const handleSearch = (e) => {
+    e.preventDefault()
+    const search = e.target.id.value
+    const searchNumber = parseInt(search)
     const result = markers.filter((marker) => {
-      return marker.id === parseInt(search)
+      return marker.id === searchNumber
     })
 
-    if(result.length > 0){
+    if(result.length > 0 && searchNumber < 62){
       setSelected(result[0])
       setOpen(true)
+    }
+
+    if(searchNumber > 62) {
+      console.log(searchNumber)
+      notify()
     }
   }
   return (
@@ -44,20 +55,24 @@ export default function Home() {
 
            {
             open && <InfoWindow position={{lat:selected.latitude, lng: selected.longitude}} onCloseClick={() => setOpen(false)}><div>
-              <p>Id: {selected.id}</p>
-              <p>Name: {selected.name}</p>
-              <p>Last cleaning date: {selected.last_cleanign_date}</p>
-              <p>Next cleaning date: {selected.next_cleaning_date}</p>
+              <p className="text-black">Id: {selected.id}</p>
+              <p className="text-black">Name: {selected.name}</p>
+              <p className="text-black">Last cleaning date: {selected.last_cleaning_date}</p>
+              <p className="text-black">Next cleaning date: {selected.next_cleaning_date}</p>
 
               </div></InfoWindow>
            }
         </Map>
         <div className="absolute top-5 left-1/3 w-full ">
-          <input type="text" className="w-1/4 h-8 rounded-sm rounded-r-none border" placeholder="enter house id" onChange={(e) => setSearh(e.target.value)}/>
-          <button className="bg-white border h-8 rounded-sm rounded-l-none" onClick={handleSearch} >Search</button>
+          <form onSubmit={handleSearch}>
+            <input type="text" name="id" className="w-1/4 h-8 rounded-sm text-black  rounded-r-none border" placeholder="enter house id" />
+            <button className="bg-white border h-8 rounded-sm rounded-l-none text-black" type="submit">Search</button>
+          </form>
         </div>
+      <ToastContainer />
+
       </div>
-      
+
     </APIProvider>
   );
 }
